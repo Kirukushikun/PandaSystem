1eabd62a4789e2effd2d7e6f7f248ef6df15a55d
Iverson Craig <148548092+Kirukushikun@users.noreply.github.com>
2025-08-08 15:18:38 +0800

    modified:   composer.lock 	modified:   package-lock.json 	modified:   package.json 	modified:   resources/css/app.css 	modified:   resources/views/layouts/app.blade.php 	new file:   storage/debugbar/.gitignore 	deleted:    vite.config.js 	new file:   vite.config.mjs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="author" content="adam">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
     {{-- Fonts --}}
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
